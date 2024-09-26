4200303a95e3331fa6b790e47aeac0f506c442b0
Meoiswa <meoiswa@gmail.com>
2024-09-26 10:50:45 +0200

    add help tooltips to bgm enable/disable headers

--- AutoBGM/AutoBGMUI.cs
+++ AutoBGM/AutoBGMUI.cs
@@ -3,6 +3,7 @@ using Dalamud.Interface.Windowing;
 using ImGuiNET;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 
 namespace AutoBGM
@@ -44,45 +45,113 @@ namespace AutoBGM
 
     private void DrawSection(string headerText, List<ConditionAction> actions, bool defaultValue)
     {
+      var toRemove = new List<ConditionAction>();
+
       ImGui.PushID(headerText);
-      if (ImGui.CollapsingHeader(headerText))
+
+      var isOpen = ImGui.CollapsingHeader(headerText);
+      ImGui.SameLine();
+      ImGui.SetCursorPosX(ImGui.GetWindowWidth() - 30);
+      // right align:
+      // Define the size and position of the circle
+      Vector2 circlePos = ImGui.GetCursorScreenPos() + new Vector2(0, 1); // Get the current cursor position
+      float radius = 10.0f;  // Set the circle radius
+      uint circleColor = ImGui.ColorConvertFloat4ToU32(new Vector4(0.6f, 0.6f, 0.6f, 1.0f)); // Gray color
+
+      // Get the current draw list for custom drawing
+      var drawList = ImGui.GetWindowDrawList();
+
+      // Draw the filled circle
+      drawList.AddCircleFilled(new Vector2(circlePos.X + radius, circlePos.Y + radius), radius, circleColor);
+
+      // Move the cursor inside the circle to draw the "?"
+      ImGui.SetCursorPosX(ImGui.GetWindowWidth() - 23); // Offset a bit to center the "?"
+      ImGui.Text("?"); // Draw the "?" inside the circle
+      ImGui.SameLine();
+      ImGui.SetCursorPosX(ImGui.GetWindowWidth() - 30); // Offset a bit to center the "?"
+
+      // Check if the mouse is hovering over the circle and display the tooltip
+      ImGui.InvisibleButton("help_icon", new Vector2(radius * 2, radius * 2)); // Create an invisible button for hover detection
+      if (ImGui.IsItemHovered())
+      {
+        ImGui.BeginTooltip();
+
+        ImGui.PushTextWrapPos(300f); // Tooltip Width
+        var text = $"BGM will be {(defaultValue ? "enabled" : "disabled")} when:\n";
+        var and = false;
+        foreach (var condtion in actions)
+        {
+          if (and)
+          {
+            text += " or ";
+          }
+          else
+          {
+            and = true;
+          }
+          text += $"{(condtion.Value ? "" : "not ")}{condtion.Condition}";
+        }
+        ImGui.TextWrapped(text);
+        ImGui.PopTextWrapPos();
+        ImGui.EndTooltip();
+      }
+
+      if (isOpen)
       {
         ImGui.Indent();
-        foreach (var condition in actions)
+        ImGui.BeginTable(headerText, 3);
+
+        ImGui.TableSetupColumn("Condition", ImGuiTableColumnFlags.WidthStretch);
+        ImGui.TableSetupColumn("Value", ImGuiTableColumnFlags.WidthFixed, 30);
+        ImGui.TableSetupColumn("Remove", ImGuiTableColumnFlags.WidthFixed, 20);
+
+        foreach (var cav in actions)
         {
-          ImGui.PushID($"{condition.Condition}");
-          ImGui.Text($"{condition.Condition}");
-          ImGui.SameLine();
-          var value = condition.Value;
+          ImGui.PushID($"{cav.Condition}");
+          ImGui.TableNextColumn();
+          ImGui.Text($"{cav.Condition}");
+          ImGui.TableNextColumn();
+          var value = cav.Value;
           if (ImGui.Checkbox("##value", ref value))
           {
-            condition.Value = value;
+            cav.Value = value;
             configuration.Save();
           }
-          ImGui.SameLine();
+          ImGui.TableNextColumn();
           if (ImGui.Button("X"))
           {
-            if (actions.Remove(condition))
-            {
-              configuration.Save();
-            }
+            toRemove.Add(cav);
           }
           ImGui.PopID();
         }
-
-        ImGui.Separator();
+        ImGui.EndTable();
 
         ImGui.Text("Add new condition:");
         var selected = 0;
         if (ImGui.Combo("##ConditionFlag", ref selected, conditionNames, conditionNames.Length))
         {
           var newCondition = (ConditionFlag)Enum.Parse(typeof(ConditionFlag), conditionNames[selected]);
-          actions.Add(new ConditionAction { Condition = newCondition, Value = defaultValue });
-          configuration.Save();
+          if (!actions.Any(c => c.Condition == newCondition))
+          {
+            actions.Add(new ConditionAction { Condition = newCondition, Value = defaultValue });
+            configuration.Save();
+          }
         }
 
-        ImGui.Unindent();
       }
+
+      var mustSave = false;
+      foreach (var cav in toRemove)
+      {
+        mustSave |= actions.Remove(cav);
+      }
+
+      if (mustSave)
+      {
+        configuration.Save();
+      }
+
+      ImGui.Unindent();
     }
 
     private void EnableSection()
